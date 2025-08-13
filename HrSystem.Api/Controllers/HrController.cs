bc19a3f545f379eddcb42618be01a6bb71f7bed0
OmarSulaiman <omarsolimanne@gmail.com>
2025-08-13 14:58:29 +0300

    Update HrController.cs

--- HrSystem.Api/Controllers/HrController.cs
+++ HrSystem.Api/Controllers/HrController.cs
@@ -18,35 +18,35 @@ public class HrController : ControllerBase
     }
 
     [HttpPost("Add one vacation")]
-    public async Task<ActionResult<VacationDto>> AddSingle(CreateVacationDto dto)
+    public async Task<ActionResult<VacationDto>> AddSingleVacation(CreateVacationDto dto)
     {
         var vacation = await _vacationService.AddSingleAsync(dto, "admin", "HR");
         return CreatedAtAction(nameof(GetById), new { id = vacation.Id }, vacation);
     }
 
     [HttpPost("Add group of vacations")]
-    public async Task<ActionResult<List<VacationDto>>> AddBatch(CreateVacationsBatchDto dto)
+    public async Task<ActionResult<List<VacationDto>>> AddBatchOfVacations(CreateVacationsBatchDto dto)
     {
         var created = await _vacationService.AddBatchAsync(dto);
         return Ok(created);
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<VacationDto>>> GetAll()
+    public async Task<ActionResult<List<VacationDto>>> GetAllVAcations()
     {
         var vacations = await _vacationService.GetAllAsync();
         return Ok(vacations);
     }
 
     [HttpGet("{id:int}")]
-    public async Task<ActionResult<VacationDto>> GetById(int id)
+    public async Task<ActionResult<VacationDto>> GetVacationById(int id)
     {
         var vacation = await _vacationService.GetByIdAsync(id);
         return vacation == null ? NotFound() : Ok(vacation);
     }
 
     [HttpDelete("{id:int}")]
-    public async Task<IActionResult> Delete(int id)
+    public async Task<IActionResult> DeleteVacation(int id)
     {
         await _vacationService.DeleteAsync(id);
         return NoContent();
